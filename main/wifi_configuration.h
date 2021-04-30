a7669f0071eb59ebcd288b59188a999f634df297
AntonioR-dev <82103887+AntonioR-dev@users.noreply.github.com>
2021-04-30 16:29:55 +0200

    Update wifi_configuration.h

--- main/wifi_configuration.h
+++ main/wifi_configuration.h
@@ -10,8 +10,8 @@
 #ifndef __WIFI_CONFIGURATION__
 #define __WIFI_CONFIGURATION__
 
-#define WIFI_SSID "DAP"
-#define WIFI_PASS "12345678"
+#define WIFI_SSID "TIM-98304470"
+#define WIFI_PASS "iTuRT3cHg6PMqspR"
 
 #define PORT 3240
 
